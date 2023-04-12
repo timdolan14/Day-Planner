b26e3f08c3e7772439c8a66fd2060282d6bf0e6c
Tim Dolan <timmydolan14@gmail.com>
2023-04-12 18:13:06 -0400

    Updates

--- Develop/script.js
+++ Develop/script.js
@@ -3,20 +3,23 @@
 // in the html.
 
 var currentHour = dayjs().format("HH");
-var timedisplayEl = $('#time-display');
+var timedisplayEl = $('#currentDay');
 
-
-
-$(".savebtn").on('click', function(){
-
-})
 
 function displayTime () {
   var today = dayjs().format('MMMM Do YYYY, h:mm:ss a');
   timedisplayEl.text(today);
 }
 
-$(function () {
+$(".savebtn").on('click', function () {
+
+});
+
+$('.time-block').each(function () {
+
+}); 
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -35,5 +38,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
-
