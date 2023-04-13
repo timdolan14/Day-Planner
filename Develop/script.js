b2c462698751c8ad22299b9632f5d5efd4e513a7
Tim Dolan <timmydolan14@gmail.com>
2023-04-13 19:02:28 -0400

    Updates to HTML & Additional JS Functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,22 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 // Display date and time on top
 $("#currentDay").text(dayjs().format('dddd MMMM DD YYYY, h:mm A'));
 
-// function hourBlocking () {
-
-var currentTime = dayjs().format('HH');
-
+var currentTime = dayjs().hour();
 
 $('.time-block').each(function () {
 
-  var plannerHour = $(".time-block").attr('id');
+  var plannerHour = parseInt($(this).attr('id').split("-")[1]);
+  console.log(plannerHour);
+  console.log(typeof plannerHour);
+  console.log(typeof currentTime);
 
   // 'this' is referring to the time-block element in the HTML file (declared above)
   // Going into the HTML to find "time-block" and its ID
   // If the plannerHour(current hour) is less than the currentTime (right now), 
   // Add past to the ID in the html
   
-  if (plannerHour > currentTime) {
+  if (plannerHour < currentTime) {
     console.log("past");
     $(this).removeClass('future');
     $(this).removeClass('present');
@@ -37,7 +34,7 @@ $('.time-block').each(function () {
     // if the plannerHour (current hour) is greater than the currentTime (right now), 
     // add future to the ID in the html
 
-  } if (plannerHour < currentTime) {
+  } if (plannerHour > currentTime) {
     console.log("future");
     $(this).addClass('future');
     $(this).removeClass('present');
@@ -45,37 +42,30 @@ $('.time-block').each(function () {
   }
 
   });
-  
 
 
-
-
-function planner () {
-
+function savePlanner () {
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+  $("#hour-18 .description").val(localStorage.getItem("hour-18"))
+  $("#hour-19 .description").val(localStorage.getItem("hour-19"))
 };
 
 $(".saveBtn").on('click', function () {
   console.log("save");
-
+  console.log($(this));
+  console.log($(this).siblings(".description").val());
+  console.log($(this).parent().attr("id"));
+  var key = $(this).parent().attr("id");
+  var endValue = $(this).siblings(".description").val();
+  localStorage.setItem(key, endValue);
 });
 
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+savePlanner();
